ae003ce5bf596f5bbd7736c9af6bd92e8c19199d
Pengfei & Xuan <mapengfei@mail.nwpu.edu.cn>
2024-07-30 00:34:54 +0800

    有模有样

---
+++ tests/test.cpp
@@ -0,0 +1,16 @@
+/// @date 2024-07-30
+/// @file test.cpp
+/// @author Ma Pengfei (code@pengfeima.cn)
+/// @version 0.1
+/// @copyright Copyright (c) 2024 Ma Pengfei
+/// 
+/// @brief 
+/// 
+///
+#include <iostream>
+#include <lib.h>
+int main(){
+    a();
+    printf("Hello World\n");
+    return 0;
+}
