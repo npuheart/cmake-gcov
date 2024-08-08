9aa732a3f151c761df97517fc8fe3a26c2e3f936
Pengfei & Xuan <mapengfei@mail.nwpu.edu.cn>
2024-08-09 03:00:22 +0800

    [feat] vtu 存储不同长度的数据

--- src/VtuIO.h
+++ src/VtuIO.h
@@ -7,56 +7,84 @@
 /// @brief
 ///
 ///
+
+#pragma once
+#include <array>
 #include <common/log.h>
 #include <pugixml.hpp>
 #include <vector>
 
+namespace {
+void processStringStream(std::stringstream& sstream, const double& x) { sstream << x << " "; }
+
+template <size_t dim, typename T>
+void processStringStream(std::stringstream& sstream, const std::array<T, dim>& x) {
+    for (size_t i = 0; i < dim; i++) {
+        sstream << x[i] << " ";
+    }
+}
+
+template <typename T>
+struct value_size_of_vector;
+
+template <typename T>
+struct value_size_of_vector<std::vector<T>> {
+    static constexpr std::size_t size = 1;
+};
+
+template <typename T, std::size_t N>
+struct value_size_of_vector<std::vector<std::array<T, N>>> {
+    static constexpr std::size_t size = N;
+};
+
+
 // 将一个vector转换成字符串
-template <typename T, template<typename> typename Vector>
-Vector<std::string> vectors_to_strings(const Vector<T> & first) {
+template <typename T, template <typename> typename Vector>
+Vector<std::string> vectors_to_strings(const Vector<T>& first) {
     Vector<std::string> result;
-    std::stringstream        sstream;
+    std::stringstream   sstream;
     for (auto i : first)
-        sstream << i << " ";
+        processStringStream(sstream, i);
     result.push_back(sstream.str());
     return result;
 }
 
-template <typename T, template<typename> typename Vector, typename... Args>
+template <typename T, template <typename> typename Vector, typename... Args>
 Vector<std::string> vectors_to_strings(const Vector<T>& first, const Args&... rest) {
     const Vector<std::string>& temp1 = vectors_to_strings(first);
     const Vector<std::string>& temp2 = vectors_to_strings(rest...);
-    Vector<std::string> temp;
+    Vector<std::string>        temp;
     for (auto i : temp1)
         temp.push_back(i);
     for (auto i : temp2)
         temp.push_back(i);
     return temp;
 }
+} // namespace
 
-template <template<typename> typename Vector>
+template <template <typename> typename Vector>
 void write_particles_to_vtu(
-    const int& length,
-    const std::string &points,
-    const std::string &filename,
-    const Vector<std::string> &contents, 
-    const Vector<std::string> &tags, 
-    const Vector<int> &value_sizes
-    )
+    const int& length, 
+    const std::string& points, 
+    const std::string& filename,
+    const Vector<std::string>& contents, 
+    const Vector<std::string>& tags,
+    const Vector<size_t>& value_sizes) 
 {
-    const int num  = contents.size();
+    const int num = contents.size();
     ASSERT(num == tags.size() && num == value_sizes.size());
     pugi::xml_document xml_node;
-    auto vtkfile_node = xml_node.append_child("VTKFile");
+    auto               vtkfile_node          = xml_node.append_child("VTKFile");
     vtkfile_node.append_attribute("type")    = "UnstructuredGrid";
     vtkfile_node.append_attribute("version") = "0.1";
     {
         auto u_node = vtkfile_node.append_child("UnstructuredGrid");
         {
-            auto piece = u_node.append_child("Piece");
+            auto piece                               = u_node.append_child("Piece");
             piece.append_attribute("NumberOfPoints") = std::to_string(length).c_str();
             piece.append_attribute("NumberOfCells")  = "0";
             {
+                // points coordinates
                 auto node_1     = piece.append_child("Points");
                 auto data_array = node_1.append_child("DataArray");
 
@@ -66,6 +94,7 @@ void write_particles_to_vtu(
                 data_array.append_child(pugi::node_pcdata).set_value(points.c_str());
             }
             {
+                // cells
                 auto cells_node                        = piece.append_child("Cells");
                 auto dataarray_1                       = cells_node.append_child("DataArray");
                 dataarray_1.append_attribute("type")   = "UInt32";
@@ -84,9 +113,10 @@ void write_particles_to_vtu(
                 dataarray_3.append_child(pugi::node_pcdata).set_value("");
             }
 
-            auto point_data                        = piece.append_child("PointData");
+            auto point_data = piece.append_child("PointData");
             // point_data.append_attribute("Vectors") = tags[0].c_str();
             for (size_t i = 0; i < num; i++) {
+                // points data
                 spdlog::info(" {}: {}", tags[i], contents[i]);
                 auto data_array = point_data.append_child("DataArray");
 
@@ -101,20 +131,30 @@ void write_particles_to_vtu(
     xml_node.save_file(filename.c_str());
 }
 
+// 文件名、点坐标、标签、内容
 template <typename T, template <typename> typename Vector, typename... Args>
-void write_particles_to_vtu(const std::string& filename, 
-              const Vector<T>& points_raw, 
-              const Vector<std::string>& tags,
-              const Args&... contents_raw) 
+void write_particles_to_vtu(
+    const std::string& filename, 
+    const Vector<T>& points_raw, 
+    const Vector<std::string>& tags,
+    const Args&... contents_raw) 
 {
     ASSERT_INFO(sizeof...(Args) == tags.size(), "{} == {}", sizeof...(Args), tags.size());
     auto points   = vectors_to_strings(points_raw)[0];
     auto contents = vectors_to_strings(contents_raw...);
-    std::vector<int> value_sizes;
-
-    for (auto i : contents)
-        value_sizes.push_back(3);
-    
-    write_particles_to_vtu(points_raw.size()/3, points, filename, contents, tags, value_sizes);
+    std::vector<size_t> value_sizes = {value_size_of_vector<Args>::size...};
+    write_particles_to_vtu(points_raw.size(), points, filename, contents, tags, value_sizes);
 }
 
+
+// #include <VtuIO.h>
+// int main(int argc, char* argv[]) {
+//     init_logging(argc, argv);
+//     std::vector<std::array<double, 3>> points = {{1, 2, 1}, {1, 2, 2}, {1, 2, 3}};
+//     std::vector<std::array<double, 3>> v1_3D  = {{1, 2, 1}, {1, 2, 2}, {1, 2, 3}};
+//     std::vector<std::array<double, 3>> v2_3D  = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+//     std::vector<double>                v3     = {1, 2, 3};
+//     std::vector<std::string>           tags{"v1", "v2", "v3"};
+//     write_particles_to_vtu("aaa.vtu", points, tags, v1_3D, v2_3D, v3);
+//     return 0;
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,16 @@
 /// @brief main runner
 ///
 ///
-#include<VtuIO.h>
+#include <VtuIO.h>
+
 int main(int argc, char* argv[]) {
     init_logging(argc, argv);
 
-    std::vector<double>      points{1, 2, 1};
-    std::vector<double>      v1{1, 2, 1};
-    std::vector<double>      v2{1, 2, 2};
-    std::vector<double>      v3{1, 2, 3};
-    std::vector<std::string> tags{"v1", "v2", "v3"};
-    write_particles_to_vtu("aaa.vtu", points, tags,v1,v2,v3);
+    std::vector<std::array<double, 3>> points = {{1, 2, 1}, {1, 2, 2}, {1, 2, 3}};
+    std::vector<std::array<double, 3>> v1_3D  = {{1, 2, 1}, {1, 2, 2}, {1, 2, 3}};
+    std::vector<std::array<double, 3>> v2_3D  = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    std::vector<double>                v3     = {1, 2, 3};
+    std::vector<std::string>           tags{"v1", "v2", "v3"};
+    write_particles_to_vtu("aaa.vtu", points, tags, v1_3D, v2_3D, v3);
     return 0;
 }
