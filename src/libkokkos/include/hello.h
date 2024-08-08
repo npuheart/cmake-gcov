78159549d77eadb2d3a8c56bc48536b465093afb
Pengfei & Xuan <mapengfei@mail.nwpu.edu.cn>
2024-08-08 16:27:11 +0800

    [feat] 启用kokkos依赖

---
+++ src/libkokkos/include/hello.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include <iostream>
+int hello(int argc, char* argv[]);
--- tools/ziran2019.sh
+++ tools/ziran2019.sh
@@ -3,3 +3,4 @@
 
 spack load /4hec75 # gcc@13.2.0
 spack env activate ziran2019 -p 
+spack env activate mpm-kokkos -p 
